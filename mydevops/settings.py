b6514d015397c7f28b0c6f4271431d2b3e9e27d4
aclstack <aclstack@gmail.com>
2018-07-11 23:21:25 +0800

    数据库信息初始化，解决Django2.0 MySQL模块冲突问题

--- mydevops/settings.py
+++ mydevops/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scanhosts',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,24 @@ WSGI_APPLICATION = 'mydevops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydevops',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {},
+        'init_command': 'SET storage_engine=INNODB,'    
+        'SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED, autocommit=1, names "utf8";',
     }
 }
 
--- scanhosts/models.py
+++ scanhosts/models.py
@@ -1,3 +1,24 @@
+# -*- coding:utf-8 -*-
 from django.db import models
 
+
 # Create your models here.
+class UserIPInfo(models.Model):
+    ip = models.CharField(max_length=40, default='', verbose_name='ip地址')
+    time = models.DateTimeField(verbose_name="更新时间", auto_now=True)
+
+    class Meta:
+        verbose_name = "用户访问地址信息表"
+        verbose_name_plural = verbose_name
+        db_table = "useripinfo"     # 定义表名称，不然默认为应用名 + 模型名
+
+
+class BrowseInfo(models.Model):
+    useragent = models.CharField(max_length=100, default='', verbose_name='用户浏览器agent', null=True)
+    models.CharField(max_length=256, verbose_name='唯一设备ID', default='')
+    userip = models.ForeignKey('UserIPInfo', on_delete=models.CASCADE)  # 定义外键 在Django 2.0 on_delete为必有参数
+
+    class Meta:
+        verbose_name = "用户浏览器信息表"
+        verbose_name_plural = verbose_name
+        db_table = "browseinfo"
